2df05406de68050dd09d72b024dc9441db3df72a
Grigoriy <grisha101202@yandex,ru>
2021-03-04 00:14:15 +0300

    ended work and go sleep

--- src/Character.java
+++ src/Character.java
@@ -1,10 +1,25 @@
 public class Character {
     private int hp;
     private String name;
-    public Character(String name, int hp) {
+    public Character(String name, int choose) {
         this.name = name;
-        this.hp = hp;
+        switch (choose) {
+            case 1:
+                hp = 5;
+                break;
+            case 2:
+                hp = 3;
+                break;
+            case 3:
+                hp = 1;
+                break;
+        }
     }
+
+    public void deleteHP () {
+        hp--;
+    }
+
     public String getName() {
         return name;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Добро пожаловать, пожалуйста представьтесь. \n" + "---------------------------------------------");
+        System.out.println("Введите своё имя: ");
+        String name = in.next();
+        System.out.println("Выберите сложность: \n" + "1) Легкая (5 жизней) \n" + "2) Средняя (3 жизни) \n" + "3) Сложная (1 жизнь) ");
+        System.out.println("Напишите пункт сложности, которую вы выбрали: ");
+        int choose = in.nextInt();
+        Character character = new Character(name, choose);
+        boolean flag = true;
+        System.out.println("Тест по знанию ООП Java состоит из 10 вопросов, для успешного прохождения требуется решить все 10 вопросов");
+        for(int i = 0; i<10; i++) {
+            if(character.getHp() < 0) {
+                flag = false;
+                break;
+            }
+        //нужно продумать класс ситуации и вопросы
+        }
+    }
 }
