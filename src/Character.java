ae4465313a6dc1fb035573733ac0d9ffb53cb60c
Grigoriy <grisha101202@yandex,ru>
2021-03-03 23:41:10 +0300

    ended class character

---
+++ src/Character.java
@@ -0,0 +1,15 @@
+public class Character {
+    private int hp;
+    private String name;
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getHp() {
+        return hp;
+    }
+
+}
