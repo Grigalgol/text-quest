63501b059c5768b7fbcbe3c68c9272f2c79ce878
Grigoriy <grisha101202@yandex,ru>
2021-03-04 17:02:23 +0300

    ended and i dont kmow what to do

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Добро пожаловать, пожалуйста представьтесь. \n" + "---------------------------------------------");
         System.out.println("Введите своё имя: ");
         String name = in.next();
-        System.out.println("Выберите сложность: \n" + "1) Легкая (5 жизней) \n" + "2) Средняя (3 жизни) \n" + "3) Сложная (1 жизнь) ");
+        System.out.println("Выберите сложность: \n" + "1) Легкая (3 жизни) \n" + "2) Средняя (2 жизни) \n" + "3) Сложная (1 жизнь) ");
         System.out.println("Напишите пункт сложности, которую вы выбрали: ");
         int choose = in.nextInt();
         Character character = new Character(name, choose);
+
+        Question[] q = new Question[2];
+
+        //составляем вопросы
+        q[0].setQuestion("Что из перечисленного не относится к принципам ООП? \n" + "1) Полиморфизм \n" + "2) Инкапсуляция \n" + "3) Консервация \n" + "4) Наследование \n");
+        q[0].setRightanswer(3);
+        q[1].setQuestion("Для сокрытия методов или переменных класса от других классов используется модификатор доступа \n" + "1) private \n" + "2) public \n" + "3) default \n" + "4) protected \n");
+        q[1].setRightanswer(1);
+
         boolean flag = true;
 
-        System.out.println("Тест по знанию ООП Java состоит из 10 вопросов, для успешного прохождения требуется решить все 10 вопросов");
-        for(int i = 0; i<10; i++) {
+        System.out.println("Тест по знанию ООП Java состоит из 5 вопросов, для успешного прохождения требуется решить все 5 вопросов");
+
+        for(int i = 0; i< q.length; i++) {
             if(character.getHp() < 0) {
                 flag = false;
                 break;
             }
-
-        //нужно продумать класс ситуации и вопросы
+            q[i].printQuestion();
+            System.out.println("Ваш ответ:");
+            q[i].setAnswer(in.nextInt());
+            if(q[i].getAnswer() == q[i].getRightanswer()) System.out.println("Ответ правильный! \n");
+            else {
+                character.deleteHP();
+                System.out.println("Ответ не правильный, вы теряете жизнь! \n");
+            }
         }
+        if(!flag) System.out.println("Вы проиграл!");
+        else System.out.println("Тест пройден, поздравляю пидрила!");
     }
 }
--- src/Question.java
+++ src/Question.java
@@ -1,16 +1,36 @@
 import java.util.ArrayList;
 
 public class Question {
-    Question[] q;
     private String question;
     private int answer, rightanswer;
 
-    public Question (String question, int rightanswer) {
-        this.rightanswer = rightanswer;
-        this.question = question;
-    }
+    public Question() {}
 
     public void setAnswer(int answer) {
         this.answer = answer;
+    }
+
+    public void setRightanswer(int rightanswer) {
+        this.rightanswer = rightanswer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public int getRightanswer() {
+        return rightanswer;
+    }
+
+    public void printQuestion() {
+        System.out.println(question);
     }
 }
